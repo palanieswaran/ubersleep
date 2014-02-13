5c7cf0a0e317dd7273c3eb72eb4f7a86fb6b6ba9
vagrant <vagrant@precise32.(none)>
2014-02-13 20:46:40 +0000

    updated index

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var project = require('./routes/project');
-var palette = require('./routes/palette');
+var schedule = require()
+// var project = require('./routes/project');
+// var palette = require('./routes/palette');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,8 +39,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/project/:id', project.projectInfo);
-app.get('/palette', palette.randomPalette);
+//app.get('/project/:id', project.projectInfo);
+//app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
