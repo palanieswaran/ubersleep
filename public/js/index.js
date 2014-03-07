8084703502580beb1ecb4dc5361e183a060b2155
vagrant <vagrant@precise32.(none)>
2014-03-07 06:49:30 +0000

    tutorial added

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	/*$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Thanks for clicking me!");
+		$(".jumbotron p").toggleClass("active");
+	});*/
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$('#startBtn').click(goBackToSchedule);
+}
